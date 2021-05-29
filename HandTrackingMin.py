54569f576183ada0c379c458a02bcfe658d4d08f
KVN2099 <kevinromerooviedo@gmail.com>
2021-05-28 21:32:17 -0600

    Finished module 1

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -1,33 +1,20 @@
 import cv2
 import mediapipe as mp
 import time
+import modules.HandTrackingModule as htm
 
 cap = cv2.VideoCapture(1)
-
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
-
 pTime = 0
 cTime = 0
 
+detector = htm.HandDetector()
+
 while True:
     success, img = cap.read()
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
-
-    # print(results.multi_hand_landmarks)
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:
-            for id, lm in enumerate(handLms.landmark):
-                # print(id, lm)
-                h, w, c = img.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
-                print(id, cx, cy)
-                if id == 0:
-                    cv2.circle(img, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
-
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img)
+    if len(lmList) != 0:
+        print(lmList[4])
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
